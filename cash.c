23f3f466feb4b23fcc44e34b6c5171367c9f1ef3
Omar-Nour <Omar-Nour@users.noreply.github.com>
2019-06-30 12:47:26 +0000

    20190630T124702Z

--- cash.c
+++ cash.c
@@ -2,33 +2,34 @@
 #include <stdio.h>
 #include <math.h>
 
-int main (void)
+int main(void)
 {
     float f;
     int cents;
     int coins = 0;
+    // make sure input is +ve
     do
     {
         f = get_float("Owing? : ");   
     }
     while (f < 0);
     cents = round(f * 100);
-    while ( cents > 24 )
+    while (cents > 24)
     {
       cents = cents - 25;
       coins++ ;    
     }
-    while ( cents > 9 )
+    while (cents > 9)
     {
       cents = cents - 10;
       coins++ ;    
     }
-    while ( cents > 4 )
+    while (cents > 4)
     {
       cents = cents - 5;
       coins++ ;    
     }
-    while ( cents > 0 )
+    while (cents > 0)
     {
       cents = cents - 1;
       coins++ ;    
