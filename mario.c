2e63eaf30c1241f37bc506ba5a6cc6a73dd63bb3
Omar-Nour <Omar-Nour@users.noreply.github.com>
2019-06-30 12:58:09 +0000

    20190630T125726Z

--- mario.c
+++ mario.c
@@ -3,23 +3,24 @@
 
 int main(void)
 {
-   int H; 
-   do  
-   {
+    // part 1 get a valid input for height
+    int H; 
+    do  
+    {
         H = get_int("Height: ");  
-   }
-   while (H < 1 || H > 8);
-   // part 2: build pyramid after validating input..
-   for (int i = 0; i < H; i++)
-   {
-       for (int dd = 1; dd < (H - i); dd++)
-       {
-           printf(" ");
-       }     
-       for (int nl = -1; nl < i; nl++)
-       {
-           printf("#");
-       }
-       printf("\n");
-   }
+    }
+    while (H < 1 || H > 8);
+    // part 2: build pyramid after validating input..
+    for (int i = 0; i < H; i++)
+    {
+        for (int dd = 1; dd < (H - i); dd++)
+        {
+            printf(" ");
+        }     
+        for (int nl = -1; nl < i; nl++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
